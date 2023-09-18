3cf877cf1c4953df44324a68c485bdffad57e985
omkeshborse <bomkes25@gmail.com>
2023-09-18 18:57:59 +0530

    feat : Hero section

---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> AnyWhere
+        </h1>
+        <p>
+          Strapi is the next-gen headless CMS, open-source, javascript, enabling
+          content-rich experiences to be created, managed and exposed to any
+          digital device
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Hero
